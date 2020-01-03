c11316bda65b92e8c3c9993c4de5e715d80626df
Ceyhun <ceyhuneren@zinnz.repos.github.com>
2020-01-03 23:41:18 +0300

    Revert ""Commit to Git""
    This reverts commit 01c73322771f17d978cb38bdf50f69b6d608c6b5.

---
+++ Assets/_TicTacGo/Scripts/Gameplay/Core/ObjectPool.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabsClock;
+    public Stack<GameObject> objectPool = new Stack<GameObject>();
+
+    private static ObjectPool _instance;
+    public static ObjectPool Initializing 
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                GameObject oPool = new GameObject("ObjectPool");
+                oPool.AddComponent<ObjectPool>();
+            }
+            return _instance;
+        }
+    
+    }
+
+    private void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Debug.Log("Destroying other ObjectPools..");
+            Destroy(gameObject);
+        }
+
+    }
+
+    /// <summary>
+    /// Why i use start instead of awake method ?
+    /// -Because, if this object is a singleton, I want it to fill
+    /// else if it will already be destroyed in awake.
+    /// </summary>
+    private void Start()
+    {
+        // fill the object pool with 35 clock
+        FillPool(35);
+    }
+
+    public GameObject GetAtPool()
+    {
+        if(objectPool.Count > 0)
+        {
+            GameObject newClock = objectPool.Pop();
+            newClock.SetActive(true);
+            return newClock;
+        }
+        return Instantiate(prefabsClock);
+    }
+
+    public void AddPool(GameObject newObject)
+    {
+        newObject.SetActive(false);
+        objectPool.Push(newObject);
+    }
+
+    public void FillPool(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            GameObject insObject = Instantiate(prefabsClock);
+            AddPool(insObject);
+        }
+    }
+
+}
---
+++ Assets/_TicTacGo/Scripts/Gameplay/WallController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    private PhysicsMaterial2D ballisticMat;
+    private bool isBallistic;
+    private GameMode gameMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameMode = GameManager.Initializing.gameMode;
+        if(gameMode == GameMode.Carrier)
+        {
+            // Enabled ballistic
+            isBallistic = true;
+
+            // Set visible wall
+            gameObject.SetActive(true);
+
+            // Set physics material
+            var myCollider = GetComponent<BoxCollider2D>();
+            myCollider.sharedMaterial = ballisticMat;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            GameManager.Initializing.GameEnd();
+            //Debug.Log("Game Over...");
+        }
+    }
+
+}
