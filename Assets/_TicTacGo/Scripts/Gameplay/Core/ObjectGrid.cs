7bdd94dbba9199c623b1e77f43b7de31d1b947b2
Ceyhun <ceyhuneren@zinnz.repos.github.com>
2020-01-03 23:39:03 +0300

    Commity to Git
    The first version included in GitHub.

---
+++ Assets/_TicTacGo/Scripts/Gameplay/Core/ObjectGrid.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid 
+{
+    public int id { get; private set; }
+
+    public float xPos { get; private set; }
+    public float yPos { get; private set; }
+    //public float zPos { get; private set; }
+    public bool isEmpty { get; set; }
+
+    public Grid(float x, float y, int gridId)
+    {
+        xPos = x;
+        yPos = y;
+        //zPos = z;
+
+        id = gridId;
+        isEmpty = true;
+    }
+
+}
+
+public class ObjectGrid : MonoBehaviour
+{
+    public List<Grid> emptyGrids = new List<Grid>();
+    public List<Grid> fullGrids = new List<Grid>();
+    
+    [SerializeField] float x_Start, y_Start;
+    [SerializeField] float x_Space, y_Space;
+    [SerializeField] int columnLength, rowLength;
+
+    private static ObjectGrid _instance;
+    public static ObjectGrid Initializing
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject oGrid = new GameObject("ObjectGrid");
+                oGrid.AddComponent<ObjectGrid>();
+            }
+            return _instance;
+        }
+
+    }
+
+    private void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Debug.Log("Destroying other ObjectPools..");
+            Destroy(gameObject);
+        }
+        CreateGrid();
+    }
+
+    // Start is called before the first frame update
+    private void CreateGrid()
+    {
+        // There is i am doing create a grid system and i am working oop(object orident programming)
+        for (int i = 0; i < columnLength * rowLength; i++)
+        {
+            var gridX = x_Start + (x_Space * (i % columnLength));
+            var gridY = y_Start + (-y_Space * (i / columnLength));
+            
+            Grid newGrid = new Grid(gridX, gridY, i);
+
+            emptyGrids.Add(newGrid);
+        }
+    }
+
+    public Vector3 EmptyPoint()
+    {
+        /*
+        // Get empty grids type of IEnumarable
+        var empty = from emptyGrid in objectGrid
+                    where emptyGrid.isEmpty
+                    select emptyGrid;
+
+        // And apply empty grids type to Array
+        Grid[] emptySlots = empty.ToArray();
+        
+        // After select a random element in empty grids type of Array
+        var point = emptySlots[Random.Range(0, emptySlots.Length)];*/
+
+        // Get empty grid in emptyGrids
+        var random = Random.Range(0, emptyGrids.Count);
+
+        var point = emptyGrids[random];
+
+        // Set selected grid is not empty
+        point.isEmpty = false;
+
+        // Move to fullgrids
+        fullGrids.Add(point);
+        emptyGrids.Remove(point);
+
+        // Finally return random empty grid vector value
+        Vector3 emptyPoint = new Vector3(point.xPos, point.yPos, point.id);
+        return emptyPoint;
+    }
+
+    public void GridEmptied(int emptiedId)
+    {
+        foreach (Grid item in fullGrids)
+        {
+            if(item.id == emptiedId)
+            {
+                emptyGrids.Add(item);
+                break;
+            }
+        }
+    }
+
+}
---
+++ Assets/_TicTacGo/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+
+public enum GameMode
+{
+    non,
+    Carrier,
+    Endless,
+}
+    
+public enum DifficultyLevel
+{
+    non,
+    Beginner,
+    Intermediate,
+    Advanced,
+}
+
+[RequireComponent(typeof(ObjectGrid))]
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] public int targetFrameRate = 60; // Application target frame per second.
+
+    public GameMode gameMode { get; set; }
+    public ClockController playerClock { get; set; }
+
+    // Prefabs
+    public GameObject Bullet;
+    //public GameObject Clock;
+
+    // My Props
+    public int bulletSpeed { get ; private set; }
+    public int highScore { get; set; }
+    public bool  iCanShoot { get; set; }
+
+    private Color Background;
+    public Color _Background 
+    {
+        get
+        {
+            return Background;
+        }
+        private set
+        {
+            Background = value;
+            Camera.main.backgroundColor = Background;
+        }
+    }
+
+    // Singleton
+    private static GameManager _instance;
+    public static GameManager Initializing
+    {
+        get
+        {
+            if (!_instance)
+            {
+                GameObject gManager = new GameObject("GameManager");
+                gManager.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        // Set bullet speed
+        bulletSpeed = 10;
+
+        // Set background color to Random Color.
+        _Background = GetRandomColor();
+
+        // VSYNC is enabled.
+        Application.targetFrameRate = targetFrameRate;
+
+        // Set _Instance to this
+        if(!_instance)
+        {
+            _instance = this;
+            //DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.Log("GameManager Destroying...");
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            SendNewClock();
+        }
+    }
+
+    /// <summary>
+    /// This function for shoot clock to direction
+    /// </summary>
+    public void HandleShoot()
+    {
+        if (playerClock)
+        {
+            playerClock.isPlayer = false;
+
+            GameObject go = Instantiate(Bullet, playerClock.BulletPosition(), Quaternion.identity);
+            go.GetComponent<Rigidbody2D>().velocity = playerClock.ShootDirection() * bulletSpeed;
+
+            ObjectPool.Initializing.AddPool(playerClock.gameObject);
+            //Eksik yer
+
+            playerClock = null;
+        }
+    }
+
+    /// <summary>
+    /// Spawn new clock on scene
+    /// </summary>
+    public void SendNewClock()
+    {
+        // Get a Clock at ObjectPool
+        var newClock = ObjectPool.Initializing.GetAtPool();
+
+        // Get a Empty at ObjectGrid
+        var clockPosition = ObjectGrid.Initializing.EmptyPoint();
+
+        // Apply on scene
+        newClock.transform.position = new Vector3(clockPosition.x, clockPosition.y, 0);
+
+        // Set grid id to new clock
+        newClock.GetComponent<ClockController>().gridId = clockPosition.z;
+    }
+
+    /// <summary>
+    /// Retry scene
+    /// </summary>
+    public void GameEnd()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+
+    Color GetRandomColor()
+    {
+        var randomColor = Random.ColorHSV(0, 1f, 0, 1f, 0, 1f);
+        return randomColor;
+    }
+}
